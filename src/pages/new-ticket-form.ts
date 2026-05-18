15a2179cc5e399e29685c84086f3618596bb91ef
Jason Harris <lord.jtharris@gmail.com>
2026-05-18 14:02:38 -0500

    Create ticket actualy create a new ticket, submit redirects after a successful ticket submission

--- src/pages/new-ticket-form.ts
+++ src/pages/new-ticket-form.ts
@@ -1,7 +1,9 @@
 import { Component, Input } from "@angular/core";
 import { FormInput } from "../components/input";
-import {FormButton} from "../components/form-button";
+import { FormButton } from "../components/form-button";
 import { FormTextArea } from "../components/textarea";
+import { Router } from "@angular/router";
+
 @Component({
     selector: "new-ticket-form",
     templateUrl: "./new-ticket-form.html",
@@ -10,29 +12,48 @@ import { FormTextArea } from "../components/textarea";
 })
 export class NewTicketForm {
 
+    router = new Router();
 
     ticketData: { [key: string]: string } = {
         title: "",
         username: "",
-        description:""
+        description: ""
     }
 
 
-    valueChange({field, value}: {field: string, value: string}) {
+    valueChange({ field, value }: { field: string, value: string }) {
         this.ticketData[field] = value;
     }
 
     onSubmit() {
-        console.log("Submitting ticket:", this.ticketData);
-    }
 
-    onCancel() {
-        console.log("Canceling ticket submission.");
-        this.ticketData = {
-            title: "",
-            username: "",
-            description: ""
-        };
-        
-    }
+        console.log("Submitting ticket:", this.ticketData);
+        try {
+            fetch('http://localhost:3000/newTicket', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.ticketData)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log("Ticket created successfully:", data);
+            })
+            .then(() => this.router.navigate(['/tickets']))
+        }
+        catch(error) {
+            console.error("Error submitting ticket:", error);
+        }
+}
+
+onCancel() {
+    console.log("Canceling ticket submission.");
+    this.ticketData = {
+        title: "",
+        username: "",
+        description: ""
+    };
+
+}
 }
